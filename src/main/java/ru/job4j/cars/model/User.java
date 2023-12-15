0969a6b0ca2b4682ef15a102e4a46f84d6ecea77
Boris <boris86msk@yandex.ru>
2023-12-15 22:54:31 +0300

    add participates table

--- src/main/java/ru/job4j/cars/model/User.java
+++ src/main/java/ru/job4j/cars/model/User.java
@@ -4,6 +4,8 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "auto_user")
@@ -18,6 +20,19 @@ public class User {
     private String login;
     @Column(name = "password")
     private String password;
+    @ManyToMany
+    @JoinTable(
+            name = "participates",
+            joinColumns = { @JoinColumn(name = "user_id") },
+            inverseJoinColumns = { @JoinColumn(name = "post_id") }
+    )
+    private List<Post> participates = new ArrayList<>();
+
+    /**
+     * name - указывает на таблицу, где идет связь вторичных ключей.
+     * joinColumns - определяет ключ родительского объекта. В данном примере User.id
+     * inverseJoinColumns - определяет ключ объекта, который мы загружаем в родительский объект (Post).
+     */
 
     public int getId() {
         return id;
